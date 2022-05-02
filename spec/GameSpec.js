a3131382f291b974bff58650e0a841005a7798fb
monenavarro <me.navarro.97@outlook.com>
2022-05-02 23:37:28 +0100

    Game applies rolls to first active frame

--- spec/GameSpec.js
+++ spec/GameSpec.js
@@ -3,17 +3,31 @@ const Game = require("../lib/Game")
 describe("Game", function(){
 
     describe("#roll", function() {
-        
-        it("implements the roll to the active frame", function() {
 
-            var inactiveFrame = { active: false }
-            var activeFrame = { active: true, roll: () => false };
-            var game = new Game([ inactiveFrame, activeFrame ]);
-            spyOn(activeFrame, 'roll');
+        var game, activeFrame1, activeFrame2;
+
+        beforeEach(function() {
+
+            var inactiveFrame = { isComplete: true }
+            activeFrame1 = { isComplete: false, roll: () => false };
+            activeFrame2 = { isComplete: false, roll: () => false };
+
+            game = new Game([ inactiveFrame, activeFrame1, activeFrame2 ]);
+            spyOn(activeFrame1, 'roll');
+            spyOn(activeFrame2, 'roll');
 
             game.roll(4);
 
-            expect(activeFrame.roll).toHaveBeenCalledWith(4);
+        });
+        
+        it("implements the roll to the first active frame", function() {
+
+            expect(activeFrame1.roll).toHaveBeenCalledWith(4);
+        });
+
+        it("does not implement the roll to the next active frame", function() {
+
+            expect(activeFrame2.roll).not.toHaveBeenCalledWith();
         });
     });
 });
