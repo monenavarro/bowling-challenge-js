1bc79427ad21fce23f0ed0486a5d91d11a9fcb68
monenavarro <me.navarro.97@outlook.com>
2022-05-02 23:07:35 +0100

    Frame tests added for complete/incomplete, passing

--- spec/FrameSpec.js
+++ spec/FrameSpec.js
@@ -8,5 +8,25 @@ describe("Frame", function() {
         frame.roll(7);
         expect(frame.getScore()).toBe(7);
     })
+
+  });
+
+  describe("#isComplete", function() {
+
+    it("is complete(true) when two rolls have been played", function() {
+
+        var frame = new Frame([7, 3]);
+        var isComplete = frame.isComplete();
+        expect(isComplete).toBe(true);
+    });
+
+    it("is incomplete(false) when less than two rolls have been played", function() {
+
+        var frame = new Frame([7]);
+        var isComplete = frame.isComplete();
+        expect(isComplete).toBe(false);
+
+    });
   })
+
 })
